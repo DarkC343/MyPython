f23c95054afce7661d3f25afc99a12d34d5add39
DarkC343 <dark.c343@gmail.com>
2018-02-11 21:26:49 +0330

    comments.py

---
+++ Quick-Start/comments.py
@@ -0,0 +1,7 @@
+# comments are normally created through # (pound sign, hashtag)
+# multi line comments begin with ''' and end with '''
+'''
+This is a multi line comment
+As you can see more here
+more and more!
+'''
